756920d87a5f0a749131f7ba60fda6b52817f1ba
Vadim Makishvili <makishvili@yandex-team.ru>
2014-04-04 23:30:33 +0400

    Генерация js файла из make block

--- tools/make-block.sh
+++ tools/make-block.sh
@@ -26,6 +26,30 @@ echo "module.exports = function (bt) {
 
 };" > blocks/$BlockName/$BlockName.bt.js
 
+echo "modules.define(
+    '$BlockName',
+    ['inherit', 'block'],
+    function (provide, inherit, YBlock) {
+        var $BlockName = inherit(YBlock, {
+            __constructor: function () {
+                this.__base.apply(this, arguments);
+
+                // здесь описываем то, что происходит сразу после создания инстанса класса
+            }
+
+            // инстанс-методы
+        }, {
+            getBlockName: function () {
+                return '$BlockName';
+            }
+
+            // статические методы
+        });
+
+        provide($BlockName);
+});
+" > blocks/$BlockName/$BlockName.js
+
 echo "-------------------------------------------------"
 echo "Создан блок blocks/$BlockName"
 echo "-------------------------------------------------"
